8172c62589d4ddbfc889412e289e916ebf26c7da
Antonio Queb <cardstillo@gmail.com>
2025-12-06 15:47:06 -0600

    feat: Agregar propiedades para selección de productos en la fila de la tabla

--- static/src/components/product_row/product_row.js
+++ static/src/components/product_row/product_row.js
@@ -30,6 +30,7 @@ ProductRow.props = {
     formatNumber: Function,
     hasSalesPermissions: { type: Boolean, optional: true },
     hasInventoryPermissions: { type: Boolean, optional: true },
+    // Props para selección
     isInCart: { type: Function, optional: true },
     toggleCartSelection: { type: Function, optional: true },
     areAllCurrentProductSelected: { type: Function, optional: true },
