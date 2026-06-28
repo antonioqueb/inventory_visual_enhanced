c9cfca4ec0dc8e39ceea1837786a2b987507d4eb
Antonio Queb <cardstillo@gmail.com>
2026-06-28 00:10:05 -0600

    feat: actualizar versión y mejorar estilo de columna en detalles del producto

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Inventario Visual Avanzado',
-    'version': '19.0.1.5.2',
+    'version': '19.0.1.5.3',
     'category': 'Inventory/Inventory',
     'summary': 'Vista visual mejorada y agrupada del inventario por producto',
     'description': """
